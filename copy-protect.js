efcffe1bf6cabfb0f907377f91b5be7282554b0e
gutchom <yskz.tg@gmail.com>
2019-12-21 18:07:25 +0900

    initial commit

---
+++ copy-protect.js
@@ -0,0 +1,54 @@
+;(function(){
+  // コピー防止オーバーレイを表示
+  function showOverlay(e) {
+    document.getElementById('overlay').classList.add('show');
+  }
+
+  // コピー防止オーバーレイを非表示
+  function hideOverlay(e) {
+    setTimeout(() => {
+      document.getElementById('overlay').classList.remove('show');
+    }, 300);
+  }
+
+  function keydown(e) {
+    // PrintScreen押下時の処理
+    if (e.code === 44) {
+      showOverlay();
+      hideOverlay();
+    }
+
+    // スクショを撮れそうな修飾キーを潰す
+    switch (e.key) {
+      case 'Control':
+      case 'Shift':
+      case 'Meta':
+      case 'Alt':
+        showOverlay();
+        break;
+
+      case 'Unidentified':
+      default:
+        hideOverlay();
+    }
+  }
+
+  // 修飾キーを話したらオーバーレイ解除
+  function keyup(e) {
+    switch (e.key) {
+      case 'Unidentified':
+      case 'Control':
+      case 'Shift':
+      case 'Meta':
+      case 'Alt':
+        hideOverlay();
+        break;
+    }
+  }
+
+  window.addEventListener('keyup', keyup);
+  window.addEventListener('keydown', keydown);
+  window.addEventListener('blur', showOverlay);
+  window.addEventListener('focus', hideOverlay);
+  window.addEventListener('beforeprint', showOverlay);
+})();
